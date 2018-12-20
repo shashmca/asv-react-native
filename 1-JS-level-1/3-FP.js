9cce40fbe050b9edf23fdc8581595b95825ffe6c
Nag-Training <nag-training@NAGABHUSHANAMs-MacBook-Pro.local>
2018-12-20 17:08:41 +0530

    todo app

--- 1-JS-level-1/3-FP.js
+++ 1-JS-level-1/3-FP.js
@@ -194,6 +194,26 @@ learnFunc()
 learnFunc()
 
 //------------------------------------------------------
+// Higher-Order-Programming
+//------------------------------------------------------
+
+function f1() {
+        console.log("f1()");
+}
+function f2() {
+        console.log('f2()');
+}
+// 
+function logWrapper(f) {
+        return function () {
+                console.log("before : LOG");
+                f();
+                console.log("after : LOG");
+        }
+}
+let f1WithLog = logWrapper(f1);
+f1WithLog();
+
 
 //------------------------------------------------------
 // Function Closures
--- 1-JS-level-1/index.js
+++ 1-JS-level-1/index.js
@@ -1,6 +1,48 @@
 "use strict"
 
-console.log("-index.js-");
+//---------------------------------------------------
+// State  ==> Todo => {id,title,completed}
+//---------------------------------------------------
 
+class Todo {
+    constructor(title) {
+        Todo.nextId++
+        this.id = Todo.nextId;
+        this.title = title;
+        this.completed = false
+    }
+}
+Todo.nextId = 0;
 
+//---------------------------------------------------
+
+class TodoService {
+    constructor() {
+        this.todos = []
+    }
+    addTodo(title) {
+        //.
+    }
+    editTodo(id, newTititle) {
+        //.
+    }
+    completeTodo(id) {
+        //.
+    }
+    completeAll() {
+        //
+    }
+    deleteTodo(id) {
+        //
+    }
+    clearCompleted() {
+        //
+    }
+    view(filter) {
+        // filter ==> ALL | ACTIVE | COMPLETED
+    }
+}
+
+const todoService = new TodoService();
+todoService.addTodo('item-1');
 
